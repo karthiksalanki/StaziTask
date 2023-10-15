612a83a246d7807f164ffee5eb446a0790c5c6c7
karthiksalanki <karthiksalanki99@gmail.com>
2023-10-16 00:44:43 +0530

    third commit

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-52tg3l*p&-&oa6p%g2im2519iu%u=ls^wx&9l9gvve&aks1h2i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','.vercel.app']
+ALLOWED_HOSTS = ['*','.vercel.app','127.0.0.1:8000']
 
 
 # Application definition
